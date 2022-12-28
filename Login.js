a62c0fe9a005b785bfaf7d97608c32f4855f673e
Woolf14 <gemilangtirto2002@gmail.com>
2022-12-28 09:37:31 +0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -2,7 +2,7 @@ function login() {
     if ($("#username").val() == "coba" && $("#password").val() == "test") {
         alert("You are a valid user" );
         // window.open("Home.html");
-        window.open("Home.html", "_self");
+        window.open("home.html", "_self");
 
     } else {
         alert("You are not a valid user");
